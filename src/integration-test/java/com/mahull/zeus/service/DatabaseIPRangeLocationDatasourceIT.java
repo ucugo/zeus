85e94404ee0d0062d1cf7dc82190360aad5fd1e9
chigozie <ucugo@yahoo.co.uk>
2018-11-26 20:00:58 +0000

    Fixed integration test build

---
+++ src/integration-test/java/com/mahull/zeus/service/DatabaseIPRangeLocationDatasourceIT.java
@@ -0,0 +1,22 @@
+package com.mahull.zeus.service;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest
+@SpringJUnitConfig
+@ActiveProfiles("integrationTest")
+@ExtendWith(SpringExtension.class)
+@Tag("integration")
+class DatabaseIPRangeLocationDatasourceIT {
+
+    @Test
+    public void test() {
+        System.out.println();
+    }
+}
